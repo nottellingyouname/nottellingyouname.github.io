8d780ee8d68126892697442a1c9eaf3df363cd0d
Ashara Dangallage don <hellzed1000@gmail.com>
2020-04-25 17:48:20 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,6 +257,22 @@
 
 })(jQuery);
 
+
+var firebaseConfig = {
+	apiKey: "AIzaSyA564kXeIeRb3yVP4EmTRFS70wqG95_UVY",
+	authDomain: "lofi-ash.firebaseapp.com",
+	databaseURL: "https://lofi-ash.firebaseio.com",
+	projectId: "lofi-ash",
+	storageBucket: "lofi-ash.appspot.com",
+	messagingSenderId: "503791388627",
+	appId: "1:503791388627:web:d64e83f754f9d60c5a5126",
+	measurementId: "G-RB66R8DG56"
+  };  
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+	const auth = firebase.auth();
+	
 function openForm() {
     document.getElementById("myForm").style.display = "block";
  }
@@ -264,3 +280,15 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+  
+	function SignUp(){
+	  var email = document.getElementById("email");
+	  var password = document.getElementById("psw");
+  
+	  const promise = auth.CreateUserWithEmailAndPassword(email.value, password.value);
+	  promise.catch(e => alert(e.message));
+  
+	  alert("Signed Up");
+  
+	}
