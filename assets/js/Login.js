2019b308c17910aac1a08239d192e27d5907a045
Ashara Dangallage don <hellzed1000@gmail.com>
2020-04-25 20:13:38 +0100

    commit

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -11,15 +11,15 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-  const auth = firebase.auth();
+const auth = firebase.auth();
 
-  function SignUp(){
-    var email = document.getElementById("email");
-    var password = document.getElementById("psw");
+function SignUp(){
+   var email = document.getElementById("email");
+   var password = document.getElementById("password");
 
-    const promise = auth.CreateUserWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
+   const promise = auth.CreateUserWithEmailAndPassword(email.value, password.value);
+   promise.catch(e => alert(e.message));
 
-    alert("Signed Up");
+   alert("Signed Up");
 
-  }
+}
