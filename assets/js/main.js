db99026fd42a48d67191fc72a125a579a4b8f82f
Ashara Dangallage don <hellzed1000@gmail.com>
2020-04-25 22:43:56 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,15 +267,28 @@ function openForm() {
 
 
 
-  const auth = firebase.auth();
+var firebaseConfig = {
+	apiKey: "AIzaSyA564kXeIeRb3yVP4EmTRFS70wqG95_UVY",
+	authDomain: "lofi-ash.firebaseapp.com",
+	databaseURL: "https://lofi-ash.firebaseio.com",
+	projectId: "lofi-ash",
+	storageBucket: "lofi-ash.appspot.com",
+	messagingSenderId: "503791388627",
+	appId: "1:503791388627:web:d64e83f754f9d60c5a5126",
+	measurementId: "G-RB66R8DG56"
+  };  
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
   
-  function SignUp(){
-	 var email = document.getElementById("email");
-	 var password = document.getElementById("password");
+const auth = firebase.auth();
   
-	 const promise = auth.CreateUserWithEmailAndPassword(email.value, password.value);
-	 promise.catch(e => alert(e.message));
+function SignUp(){
+    var email = document.getElementById("email");
+	var password = document.getElementById("password");
   
-	 alert("Signed Up");
+	const promise = auth.CreateUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
   
-  }
+	alert("Signed Up");
+  
+}
