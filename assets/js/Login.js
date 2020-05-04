8b0ec706ac1a8d7021fa22a3847d4a5cfef652c4
Ashara Dangallage don <hellzed1000@gmail.com>
2020-05-04 12:34:30 +0100

    commit

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -30,3 +30,12 @@ signupForm.addEventListener("submit", (e)=>  {
   });
 
 });
+
+//logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user signed out");
+  });
+});
