1db49528b7dbfab8cb0dc56b9863dc94f1ce271f
Ashara Dangallage don <hellzed1000@gmail.com>
2020-05-11 13:44:35 +0100

    commit

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -41,8 +41,8 @@ signupForm.addEventListener("submit", (e)=>  {
 
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     const modal = document.querySelector("#myForm");
-    M.modal.getInstance(modal).close();
-    console.log(M.modal)
+    M.Modal.getInstance(modal).close();
+    console.log(M.Modal)
     signupForm.reset();
   });
 
