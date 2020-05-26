2ddc498dbcf488094a81f84ef9585dbc1b7e8d78
Ashara Dangallage don <hellzed1000@gmail.com>
2020-05-26 18:18:50 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,3 +312,21 @@ const SetupPosts = (data) => {
 	Postlist.innerHTML = html;
 
 }
+
+
+// abhdsbdsjdbch
+
+Vue.component('blog-card', {
+	template: '#blog-card',
+	data: () => ({
+	  name: '10 Best Things to Do in Seattle',
+	  category: 'Travel',
+	  image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1159990/pike-place.jpg',
+	  author: 'Katherine Kato',
+	  desc: `Seattle is a seaport city on the west coast of the United States...`
+	})
+  });
+  
+  new Vue({
+	el: '#container'
+  });
